03593ef4db8401c9afc47152761c7f9b687de9d2
Sabrina Blockwel <sblockwel@gmail.com>
2020-10-03 23:56:50 -0300

    Melhorado lógica para não permitir placares maiores que 1000 ou menores que 0.

--- Modelos/Jogo.cs
+++ Modelos/Jogo.cs
@@ -1,9 +1,23 @@
-﻿namespace AppBasquete.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppBasquete.Models
 {
     public class Jogo
     {
+        private int placar;
         public int Id { get; set; }
-        public int Placar { get; set; }
+        public int Placar
+        {
+            get => placar;            
+            set
+            {
+                if (value < 0 && value > 1000)
+                {
+                    throw new System.Exception("O valor não pode ser inferior a 0 e não maior que 1000");
+                }
+                placar = value;
+            }
+        }
         public Jogador Jogador { get; set; }
     }
 }
